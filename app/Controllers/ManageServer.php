e6f8b1383a726cff0068dea616564fd88d6693d3
Yanick Lafontaine <yanicklafontaine@gmail.com>
2018-03-04 14:16:58 -0500

    Test /use/local

---
+++ app/Controllers/ManageServer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (C) 2014 - 2017 Threenity CMS - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary  and confidential
+ * Written by : nicelife90 <yanicklafontaine@gmail.com>
+ * Last edit : 2018
+ *
+ *
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: Yanick Lafontaine
+ * Date: 2018-03-04
+ * Time: 14:04
+ */
+
+namespace ThreenityCMS\Controllers;
+
+
+class ManageServer
+{
+
+    /**
+     * Validate if server directory exist
+     *
+     * @return bool
+     */
+    public static function valiateServerInstall()
+    {
+        if (file_exists('/user/local/WoWServer')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
--- views/manage-server.php
+++ views/manage-server.php
@@ -2,13 +2,15 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/header.php';
 
 use ThreenityCMS\Helpers\Path;
+use ThreenityCMS\Controllers\ManageServer;
 
 ?>
     <div class="content-wrapper">
         <section class="content-header">
             <h1 id="module">Core Management</h1>
             <ol class="breadcrumb">
-                <li><a href="<?php echo Path::module(); ?>/manage-server.php"><i class="fa fa-dashboard"></i>Core Management</a>
+                <li><a href="<?php echo Path::module(); ?>/manage-server.php"><i class="fa fa-dashboard"></i>Core
+                        Management</a>
                 </li>
 
             </ol>
@@ -23,12 +25,25 @@ use ThreenityCMS\Helpers\Path;
                         <div class="box-header with-border">
                             <h3 class="box-title">Manage</h3>
                         </div>
-                     
+
                         <div class="box-body">
                             <div class="row">
                                 <div class="col-md-12">
 
-                                    <!-- CONTENT HERE -->
+                                    <?php
+                                    if (!ManageServer::valiateServerInstall()) {
+                                        ?>
+                                        <div class="alert alert-warning">
+                                            Unable to detect a TrinityCore installation. <br/>
+                                            Press the <strong>Install TrinityCore</strong> button to start the
+                                            compilation process.
+                                        </div>
+                                        <div class="text-center">
+                                            <a href="manage-server.php?compile" class="btn btn-success btn-lg btn-flat"><i
+                                                        class="fa fa-plus-circle"></i> Install
+                                                TrinityCore</a>
+                                        </div>
+                                    <?php } ?>
                                 </div>
                             </div>
                         </div>
